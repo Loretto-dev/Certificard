a9225434d3e6145fe06ca2fe4731de5a9fc2be8d
Elivelton A Viveiro <eliveltonalves18@gmail.com>
2021-08-18 15:12:18 -0300

    Adicionado os arquivos do meu certificado

---
+++ Certificard.js
@@ -0,0 +1,10 @@
+//HTML
+//h1, h2, h3.....: Titulos ou subtitulos
+//div: usada para dividir a pagina em blocos
+//ol: descrição para começar uma lista ordenada
+
+
+//CSS
+//Verificar no MDN(Documentação) o que vc precisa colocar
+//background-color: cor de fundo
+
